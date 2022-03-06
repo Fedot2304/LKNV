997ccd0cab8fa455cadf32332bd32421b07b5007
Fedot2304 <100525980+Fedot2304@users.noreply.github.com>
2022-03-06 18:38:37 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const sliderline = document.querySelector('#slider-line');
 
 document.querySelector('#next').addEventListener('click', function () {
 	offset = offset + 534; 
-	if (offset>1602) {
+	if (offset>2136) {
 		offset = 0;
 	}
 	sliderline.style.left = -offset + 'px';
@@ -12,7 +12,24 @@ document.querySelector('#next').addEventListener('click', function () {
 document.querySelector('#prew').addEventListener('click', function () {
 	offset = offset - 534; 
 	if (offset<0) {
-		offset = 1602;
+		offset = 2136;
+	}
+	sliderline.style.left = -offset + 'px';
+});
+
+
+document.querySelector('#next_1').addEventListener('click', function () {
+	offset = offset + 414; 
+	if (offset>1656) {
+		offset = 0;
+	}
+	sliderline.style.left = -offset + 'px';
+});
+
+document.querySelector('#prew_1').addEventListener('click', function () {
+	offset = offset - 414; 
+	if (offset<0) {
+		offset = 1656;
 	}
 	sliderline.style.left = -offset + 'px';
 });
